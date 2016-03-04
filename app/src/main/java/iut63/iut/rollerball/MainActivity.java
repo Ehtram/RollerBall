d918b892fd2b420b66368a5a2909367a9a76870d
Quentin <quentin.laplanche@outlook.fr>
2016-03-04 09:38:12 +0100

    Get Gyroscope

--- app/src/main/java/iut63/iut/rollerball/MainActivity.java
+++ app/src/main/java/iut63/iut/rollerball/MainActivity.java
@@ -1,13 +1,23 @@
 package iut63.iut.rollerball;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+       
     }
 }
