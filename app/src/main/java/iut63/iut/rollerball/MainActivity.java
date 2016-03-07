3c09eb49cc489c385e304697e61efcc3d1cecac3
Quentin <quentin.laplanche@outlook.fr>
2016-03-07 16:15:27 +0100

    Adding Gyroscope try

--- app/src/main/java/iut63/iut/rollerball/MainActivity.java
+++ app/src/main/java/iut63/iut/rollerball/MainActivity.java
@@ -2,11 +2,14 @@ package iut63.iut.rollerball;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
@@ -18,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    public void onSensorChanged ( SensorEvent event){
+            ((TextView)findViewById(R.id.mtextView1)).setText(String.valueOf(event.values[0]));
+            ((TextView)findViewById(R.id.mtextView2)).setText(String.valueOf(event.values[1]));
+            ((TextView)findViewById(R.id.mtextView3)).setText(String.valueOf(event.values[2]));
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
