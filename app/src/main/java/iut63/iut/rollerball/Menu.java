4ecf1ee726b7ba665d0255bfeb0b1a753615ffc7
Quentin <quentin.laplanche@outlook.fr>
2016-03-19 19:46:01 +0100

    La connection avance un peu

--- app/src/main/java/iut63/iut/rollerball/Menu.java
+++ app/src/main/java/iut63/iut/rollerball/Menu.java
@@ -1,33 +1,28 @@
 package iut63.iut.rollerball;
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.nfc.Tag;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.drive.Drive;
-import com.google.android.gms.games.Game;
 import com.google.android.gms.games.Games;
 
-public class Menu extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+public class Menu extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private Button start;
     private Button leaderboard;
     private GoogleApiClient mGoogleApiClient;
     private static String TAG = Menu.class.getSimpleName();
+    private static final int RC_SIGN_IN = 9001;
+    private boolean mResolvingConnectionFailure = false;
+    private boolean mAutoStartSignInflow = true;
+    private boolean mSignInClicked = false;
 
 
     @Override
@@ -42,17 +37,81 @@ public class Menu extends AppCompatActivity implements View.OnClickListener, Goo
                 startActivity(intent);
             }
         });
+
         setLeaderboardOnClickListener();
         setGoogleApiClient();
         setGoogleSignButton();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+        Log.d(TAG,String.valueOf(mGoogleApiClient.isConnected()));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "onConnected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // Attempt to reconnect
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "onConnectionFailed");
+
+        // If the connection failed, in most of time this means user hasn't logined yet
+        // In this case invoke the `startResolutionForResult` will launch the login dialog and account picker
+
+        if (connectionResult.hasResolution()) {
+            Log.d(TAG,connectionResult.toString());
+            try {
+                connectionResult.startResolutionForResult(this, 1001);
+            }
+            catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        }else {
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(),this,RESULT_OK);
+            GooglePlayServicesUtil.showErrorDialogFragment(connectionResult.getErrorCode(),this,RESULT_OK);
+        }
+    }
+
+    private void signIn(View view) {
+//        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+        if (view.getId() == R.id.sign_in_button) {
+            // start the asynchronous sign in flow
+            mSignInClicked = true;
+            mGoogleApiClient.connect();
+        }
+        else if (view.getId() == R.id.sign_out_button) {
+            // sign out.
+            mSignInClicked = false;
+            Games.signOut(mGoogleApiClient);
+
+            // show sign-in button, hide the sign-out button
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+        }
     }
 
     private void setGoogleSignButton() {
         findViewById(R.id.sign_in_button).setOnClickListener(this);
         findViewById(R.id.sign_out_button).setOnClickListener(this);
     }
-
-
 
     private void setGoogleApiClient(){
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -77,11 +136,12 @@ public class Menu extends AppCompatActivity implements View.OnClickListener, Goo
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn(v);
+                break;
+            // ...
+        }
     }
 
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.d(TAG,"Erreur Inrésolvable"+ String.valueOf(connectionResult));
-    }
 }
