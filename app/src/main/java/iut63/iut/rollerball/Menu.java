3138d085f7908262f9881675216f686fe22f80ec
Quentin <quentin.laplanche@outlook.fr>
2016-03-21 08:43:52 +0100

    Ready for merge

--- app/src/main/java/iut63/iut/rollerball/Menu.java
+++ app/src/main/java/iut63/iut/rollerball/Menu.java
@@ -1,31 +1,17 @@
 package iut63.iut.rollerball;
 
 import android.content.Intent;
-import android.content.IntentSender;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.games.Games;
 import com.google.android.gms.games.Games;
 import com.google.example.games.basegameutils.BaseGameActivity;
 
 public class Menu extends BaseGameActivity implements View.OnClickListener {
     private Button start;
     private Button leaderboard;
-    private static  GoogleApiClient mGoogleApiClient;
     private static String TAG = Menu.class.getSimpleName();
-    private static final int RC_SIGN_IN = 9001;
-    private boolean mResolvingConnectionFailure = false;
-    private boolean mAutoStartSignInflow = true;
-    private boolean mSignInClicked = false;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,23 +27,12 @@ public class Menu extends BaseGameActivity implements View.OnClickListener {
         });
 
         setLeaderboardOnClickListener();
-        setGoogleApiClient();
         setGoogleSignButton();
     }
-
 
     private void setGoogleSignButton() {
         findViewById(R.id.sign_in_button).setOnClickListener(this);
         findViewById(R.id.sign_out_button).setOnClickListener(this);
-    }
-
-    private void setGoogleApiClient(){
-//        mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                .enableAutoManage(this /* FragmentActivity */,
-//                        this /* OnConnectionFailedListener */)
-//                .addApi(Games.API)
-//                .addScope(Games.SCOPE_GAMES)
-//                .build();
     }
 
     private void setLeaderboardOnClickListener() {
@@ -65,9 +40,11 @@ public class Menu extends BaseGameActivity implements View.OnClickListener {
         leaderboard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivityForResult(Games.Leaderboards.getLeaderboardIntent(
-                                getApiClient(), getString(R.string.leaderboard_id)),
-                        2);
+                if (getApiClient().isConnected()) {
+                    startActivityForResult(Games.Leaderboards.getLeaderboardIntent(
+                                    getApiClient(), getString(R.string.leaderboard_id)),
+                            2);
+                }
                 if(getApiClient().isConnected()){
                     Games.Leaderboards.submitScore(getApiClient(),
                             getString(R.string.leaderboard_id),
@@ -76,7 +53,6 @@ public class Menu extends BaseGameActivity implements View.OnClickListener {
             }
         });
     }
-
 
     @Override
     public void onClick(View v) {
