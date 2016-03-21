d0806fdf1bbdb83713a62fe8601333bbbd7323bb
Quentin <quentin.laplanche@outlook.fr>
2016-03-21 17:54:15 +0100

    Ajout d'un peu de documentation relatif au Google Play notamment.

--- app/src/main/java/iut63/iut/rollerball/Menu.java
+++ app/src/main/java/iut63/iut/rollerball/Menu.java
@@ -8,11 +8,21 @@ import android.widget.Button;
 import com.google.android.gms.games.Games;
 import com.google.example.games.basegameutils.BaseGameActivity;
 
+
+/**
+ * Classe de Menu, premiere Activity ouverte,
+ * extends de BaseGameActivity ( librairie de Google ),
+ * Afin de permettre la connexion au google play services.
+ */
 public class Menu extends BaseGameActivity implements View.OnClickListener {
     private Button start;
     private Button leaderboard;
     private static String TAG = Menu.class.getSimpleName();
 
+    /**
+     * Méthode appelée à chaque création de l'activité
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +40,21 @@ public class Menu extends BaseGameActivity implements View.OnClickListener {
         setGoogleSignButton();
     }
 
+    /**
+     * Méthode qui set les listener pour les boutons de connexions de Google
+     * Ici ce Listener est notre activité, car elle extends BaseGameActivity,
+     * et possede ainsi tous ce qu'il faut pour se connecter efficacement.
+     */
     private void setGoogleSignButton() {
         findViewById(R.id.sign_in_button).setOnClickListener(this);
         findViewById(R.id.sign_out_button).setOnClickListener(this);
     }
 
+    /**
+     * Définit le listener du bouton LeaderBoard,
+     * qui permet de Visualiser le classement.
+     * Pour cela, celui ci réalise un appel à startActivityForResult en lui passant l'apiGoogle et l'id du leaderboard voulu.
+     */
     private void setLeaderboardOnClickListener() {
         leaderboard = (Button) findViewById(R.id.buttonLeaderboard);
         leaderboard.setOnClickListener(new View.OnClickListener() {
@@ -54,6 +74,11 @@ public class Menu extends BaseGameActivity implements View.OnClickListener {
         });
     }
 
+    /**
+     * Méthode appelé seulement par les boutons de connexions de Google.
+     * Si la View sender est le Sign-In Button, on lance le protocole de connexion du Google Play
+     * @param v View sender du onClick
+     */
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.sign_in_button) {
@@ -65,12 +90,22 @@ public class Menu extends BaseGameActivity implements View.OnClickListener {
             findViewById(R.id.sign_out_button).setVisibility(View.GONE);
         }
     }
+
+    /**
+     * Appelé lorsque le joueur s'est correctement connecté.
+     * Fait alors disparaitre le Bouton Sign-In.
+     * Et fait apparaitre le bouton de déconnexion.
+     */
     @Override
     public void onSignInSucceeded() {
         findViewById(R.id.sign_in_button).setVisibility(View.GONE);
         findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
     }
 
+    /**
+     * Appelé lorsque le joueur n'a pas réussi à se connecter.
+     * Mets le Bouton de Sign-In visible, et fait disparaitre le bouton de déconnexion
+     */
     @Override
     public void onSignInFailed() {
         findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
